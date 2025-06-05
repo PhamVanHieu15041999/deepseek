b8141fcdd4172c2c9caf65a2aa398b6ade539433
Hieu Pham <phamvanhieu150499@gmail.com>
2025-06-06 06:55:35 +0700

    first commit

---
+++ model/User.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    _id: { type: String, require: true },
+    name: { type: String, require: true },
+    email: { type: String, require: true },
+    image: { type: String, require: false },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model.User || mongoose.model("User", UserSchema);
+export default User;
